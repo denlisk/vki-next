45015977e02f5caf4384615e7d77a841470b14fb
denlisk <160236126+denlisk@users.noreply.github.com>
2025-12-03 21:48:19 +0700

    test

--- src/app/api/students/[id]/route.ts
+++ src/app/api/students/[id]/route.ts
@@ -1,11 +1,11 @@
 import { deleteStudentDb } from '@/db/studentDb';
 import { type NextRequest } from 'next/server';
 
-export async function DELETE(request: NextRequest, context: { params: Promise<{ Id: number }> }): Promise<void | Response> {
+export async function DELETE(request: NextRequest, context: { params: Promise<{ Id: string }> }): Promise<void | Response> {
   const p = await context.params;
   const studentId = await p.Id;
 
-  const deletedStudentId = await deleteStudentDb(studentId);
+  const deletedStudentId = await deleteStudentDb(Number(studentId));
 
   return new Response(JSON.stringify({ deletedStudentId }), {
     headers: {
